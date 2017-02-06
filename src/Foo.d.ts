97c47e2732accde96a63a06164a54ca5bf811320
Pavel Birukov <birukov.pavel@gmail.com>
2017-02-06 19:26:17 +0200

    Initial commit with issue kulshekhar/ts-jest#112

---
+++ src/Foo.d.ts
@@ -0,0 +1,3 @@
+declare const enum Foo {
+    Bar = 1,
+}
---
+++ src/indexSpec.ts
@@ -0,0 +1,7 @@
+import { Bar } from "./";
+
+describe("Bar", () => {
+    it("is here", () => {
+        expect(Bar).toBeDefined();
+    });
+});
